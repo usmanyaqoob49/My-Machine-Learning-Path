3a5a64b7c6ed02bd4bc3fd2690f4e27a9f4f76df
Usman Yaqoob <68185452+usmanyaqoob49@users.noreply.github.com>
2022-07-06 22:55:24 +0500

    Add files via upload

---
+++ 2. Programming/Programming for everybody-Python/Code/if.py
@@ -0,0 +1,10 @@
+x = input("Enter a number:")
+#firstly converting string to integer
+x = int(x)
+#x == 0 means does x is equal to zero
+if x == 0:
+    print("x is equals to zero.")
+if x > 0:
+    print("x is a positive number.")
+if x < 0:
+    print("x is a negative number.")
