5a3838af805fae1d37760770fd3823f080aeb513
Usman Yaqoob <68185452+usmanyaqoob49@users.noreply.github.com>
2022-07-06 22:55:09 +0500

    Add files via upload

---
+++ 2. Programming/Programming for everybody-Python/Code/functionparameters.py
@@ -0,0 +1,10 @@
+def chat(str):
+    if str == "Hello":
+        print("Hi.")
+    elif str == "How are you?":
+        print("I am fine.")
+    else:
+        print("Go away.")
+chat("Hello")
+chat("How are you?")
+chat("wssup")
