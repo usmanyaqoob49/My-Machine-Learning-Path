fafa74dab539147d0abcd4518fb48a4cf5468775
Usman Yaqoob <68185452+usmanyaqoob49@users.noreply.github.com>
2022-07-06 22:55:38 +0500

    Add files via upload

---
+++ 2. Programming/Programming for everybody-Python/Code/istfunction.py
@@ -0,0 +1,12 @@
+def thing():
+    print("Hello World.")
+print("We are going to call the function.")
+#function calling
+thing()
+print("Function has been called.")
+#We have a build in function max() that retrun maximum number or a alphabet of string that exist in end
+#e.g if we pass xyz in arguement it will return z
+large = max("123")
+print("Max number is",large)
+largea = max("xyz")
+print("Alphabet is:",largea)
