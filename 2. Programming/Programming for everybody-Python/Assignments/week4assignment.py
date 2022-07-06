50a36d16f6f8884828b942d641966c5c46705c5d
Usman Yaqoob <68185452+usmanyaqoob49@users.noreply.github.com>
2022-07-06 22:58:11 +0500

    Add files via upload

---
+++ 2. Programming/Programming for everybody-Python/Assignments/week4assignment.py
@@ -0,0 +1,6 @@
+#Write a program that uses raw_input to prompt a user for their name and then welcomes them.
+#Note that raw_input will pop up a dialog box.
+#Enter Sarah in the pop-up box when you are prompted so
+#your output will match the desired output.
+raw_input = input("Enter Your name: ")
+print("Welcome",raw_input)
